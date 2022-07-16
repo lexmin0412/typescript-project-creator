30766714810dfc8376fc5aa79ddc35254e233ac1
lexmin0412 <zhangle_dev@outlook.com>
2022-07-17 01:52:54 +0800

    docs(vitepress config): add more theme configs

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -6,18 +6,39 @@ export default defineConfig({
 	base: '/typescript-project-creator/',
 	lastUpdated: true,
 	themeConfig: {
+		footer: {
+			message: 'Created By Lexmin0412',
+		},
 		siteTitle: 'TPC',
+		logo: 'https://via.placeholder.com/1500/ffffff/45aafa/?text=TPC',
 		nav: [
-			{ text: 'Home', link: '/' },
+			{ text: 'Github', link: 'https://github.com/lexmin0412/typescript-project-creator' },
 			{
-				text: 'More tools',
+				text: 'More Tools',
 				items: [
+					{
+						text: 'MPP',
+						link: 'https://github.com/lexmin0412/make-pkg-perfect'
+					},
 					{
 						text: 'Toolbar',
 						link: 'https://lexmin0412.github.io/advanced-stack-park/'
+					},
+					{
+						text: 'Awesome Wheels',
+						link: 'https://lexmin0412.github.io/awesome-wheels/'
 					}
 				]
-			}
+			},
+			{
+				text: 'Find me',
+				items: [
+					{
+						text: 'Github',
+						link: 'https://github.com/lexmin0412'
+					}
+				]
+			},
 		],
 		sidebar: [
 			{
@@ -31,5 +52,9 @@ export default defineConfig({
 			}
 		],
 		lastUpdatedText: 'Last Updated',
+		editLink: {
+      pattern: 'https://github.com/lexmin0412/typescript-project-creator/edit/master/docs/:path',
+      text: 'Edit this page on GitHub'
+    },
 	}
 })
